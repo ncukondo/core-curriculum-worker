bdaceb2e6688c555be740b54233aaf1278a39480
Takeshi Kondo <ncukondo@gmail.com>
2022-04-12 06:23:20 +0000

    ignore python directory

--- python/lib/google_spread.py
+++
@@ -1,9 +0,0 @@
-import gspread
-from lib.google_auth import get_creds
-
-_sheet_app:gspread.Client=None
-def get_sheetapp()->gspread.Client:
-    global _sheet_app
-    if _sheet_app==None:
-        _sheet_app=gspread.authorize(get_creds())
-    return _sheet_app
--- python/lib/utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-DATA_DIR= os.path.join(BASE_DIR,"data_in_github")
-TABLES_DIR= os.path.join(DATA_DIR,"tables_formatted")
-TABLES_INDEX= os.path.join(DATA_DIR,"table_index.csv")
-OUTPUT_DIR= os.path.join(BASE_DIR,"output")
